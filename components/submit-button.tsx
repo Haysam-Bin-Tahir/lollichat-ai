db4af90beac3f411222a21d74dbf6e3a55ee37d0
Muzamil Hussain <muzamilsethar@gmail.com>
2025-04-04 01:35:07 +0500

    fix build issue

--- components/submit-button.tsx
+++ components/submit-button.tsx
@@ -6,20 +6,24 @@ import { LoaderIcon } from '@/components/icons';
 
 import { Button } from './ui/button';
 
+interface SubmitButtonProps {
+  children: React.ReactNode;
+  isSuccessful: boolean;
+  disabled?: boolean;
+}
+
 export function SubmitButton({
   children,
   isSuccessful,
-}: {
-  children: React.ReactNode;
-  isSuccessful: boolean;
-}) {
+  disabled,
+}: SubmitButtonProps) {
   const { pending } = useFormStatus();
 
   return (
     <Button
       type={pending ? 'button' : 'submit'}
       aria-disabled={pending || isSuccessful}
-      disabled={pending || isSuccessful}
+      disabled={pending || isSuccessful || disabled}
       className="relative"
     >
       {children}
